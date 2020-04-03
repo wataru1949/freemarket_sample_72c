7977b951e24629a1652a4046e745010c6dccce25
Naoki <lmn.bsk210@gmail.com>
2020-04-03 20:45:17 +0900

    layout adjustment
    レイアウト調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  # 以下は今後使用予定のためコメントアウトしている
-  # root "buys#new"
-  root "users#show"
-  # 以下は今後使用予定のためコメントアウトしている
-  # root "items#index"
+  root "items#index"
+  resources :user, only: [:show]
   resources :items do
     resources :buy, only: [:new]
   end
