08c3aede64f8f4c758676aa23bac930a95855592
Masahiro-Iwai <iwwawwi@icloud.com>
2020-04-06 12:16:22 +0900

    200406-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  # root "users#logout"
   # 以下は今後使用予定のためコメントアウトしている
+  root "users#card"
   # root "buys#new"
-  root "users#show"
-  # 以下は今後使用予定のためコメントアウトしている
+  # root "users#show"
   # root "items#index"
   resources :items do
     resources :buy, only: [:new]
