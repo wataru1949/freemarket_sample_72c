5b61e7b11cec27c16ada5cfdd86e3fab2044b942
Masahiro-Iwai <iwwawwi@icloud.com>
2020-04-07 13:08:30 +0900

    add-card&logout-markup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "items#index"
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    get 'card', on: :member
+    get 'logout', on: :member
+  end
   resources :items do
     resources :buys, only: [:new]
   end
