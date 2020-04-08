0df657a801fc533987e36a7e0a3a22cb34cc289c
wataru1949 <e.phedra58@icloud.com>
2020-04-08 20:59:31 +0900

    edit action

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  validates :name, :detail, :price, :state, :condition, :shipping_cost, :shipping_date, :shipping_method, presence: true
+  validates :name, :detail, :price, :state, :condition, :shipping_cost, :shipping_date, :shipping_method, :item_images, presence: true
   validates :name, length: { maximum: 40 }
   validates :detail, length: { maximum: 1000 }
   validates :price, numericality: { greater_than_or_equal_to: 300 }
@@ -9,11 +9,12 @@ class Item < ApplicationRecord
   validates :state, numericality: { greater_than_or_equal_to: 0 }
   validates :state, numericality: { less_than_or_equal_to: 3 }
   
-  has_many :item_images
-
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :condition
   belongs_to_active_hash :shipping_cost
   belongs_to_active_hash :shipping_method
   belongs_to_active_hash :shipping_date
+  
+  has_many :item_images, dependent: :destroy
+  accepts_nested_attributes_for :item_images, allow_destroy: true
 end
--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,5 +1,14 @@
 class ItemImage < ApplicationRecord
+  
+  mount_uploader :image, ImageUploader
   belongs_to :item
 
-  mount_uploader :image, ImageUploader
-end
+# 枚数制限のメソッド
+  # validates :check_number_of_item_images
+
+  # def check_number_of_item_images
+  #   if item && item.item_images.count > 10
+  #   errors.add(:item, "枚数オーバー")
+  #   end
+  # end
+end 
