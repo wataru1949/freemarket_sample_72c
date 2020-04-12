17e5f2c3ae9afbd2d18f42ee3c51d9295df2d99d
wataru1949 <e.phedra58@icloud.com>
2020-04-12 18:33:13 +0900

    add edit function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,18 +21,29 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.user_id == current_user.id
+      if @item.update(item_params)
+        redirect_to root_path
+      else
+        render :edit
+      end
+    end
   end
 
   def destroy
+    item = Item.find(params[:id])
+    item.destroy
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :detail, :price, :brand, :condition_id, :shipping_cost_id, :shipping_method_id, :prefecture_id, :shipping_date_id, item_images_attributes: [:image]).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :detail, :price, :brand, :condition_id, :shipping_cost_id, :shipping_method_id, :prefecture_id, :shipping_date_id, :item_id, item_images_attributes: [:image, :_destroy, :id]).merge(user_id: current_user.id )
   end
 
 end
