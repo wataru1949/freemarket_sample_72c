7149ea73b7e698b1e9f39ec2d688f4274063fb19
wataru1949 <e.phedra58@icloud.com>
2020-04-19 21:52:16 +0900

    add varidates prefecture

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  validates :name, :detail, :price, :state, :condition, :shipping_cost, :shipping_date, :shipping_method, :item_images, presence: true
+  validates :name, :detail, :price, :state, :condition, :prefecture, :shipping_cost, :shipping_date, :shipping_method, :item_images, presence: true
   validates :name, length: { maximum: 40 }
   validates :detail, length: { maximum: 1000 }
   validates :price, numericality: { greater_than_or_equal_to: 300 }
@@ -18,4 +18,5 @@ class Item < ApplicationRecord
   has_many :item_images, dependent: :destroy
   accepts_nested_attributes_for :item_images, allow_destroy: true
   belongs_to :user
+  belongs_to :category
 end
